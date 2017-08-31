742a0b75ac3d6de1b2558e06512839bd84d10ef9
Landon Vago-Hughes <landon2008@hotmail.com>
2017-08-31 16:12:14 +0100

    searchBar cloned

---
+++ searchBarFun/searchBarFun/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  searchBarFun
+//
+//  Created by Landon Vago-Hughes on 31/08/2017.
+//  Copyright © 2017 Landon Vago-Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
